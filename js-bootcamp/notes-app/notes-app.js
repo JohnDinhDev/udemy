3e802519a94e6305c8d188f9e40060c2467d8278
John Dinh <johnduydinh@gmail.com>
2019-04-07 22:56:03 -0400

    Complete 'Rendering Our Filtered Data' lesson

--- js-bootcamp/notes-app/notes-app.js
+++ js-bootcamp/notes-app/notes-app.js
@@ -9,6 +9,28 @@ const notes = [{
     body: 'Get a new gaming chair',
 },];
 
+const notesDivEl = document.querySelector('#notes');
+
+const filters = {
+    searchText: '',
+}
+
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter( note => {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
+    });
+
+    notesDivEl.innerHTML = '';
+
+    filteredNotes.forEach( note => {
+        const noteEl = document.createElement('p');
+        noteEl.textContent = note.title;
+        notesDivEl.appendChild(noteEl);
+    })
+}
+
+renderNotes(notes, filters);
+
 document.querySelector('#create-note').addEventListener('click', e => {
     e.target.textContent = 'Hello World';
 });
@@ -20,5 +42,6 @@ document.querySelector('#remove-all').addEventListener('click', e => {
 });
 
 document.querySelector('#search-text').addEventListener('input', e => {
-    console.log(e.target.value);
-})
+    filters.searchText = e.target.value;
+    renderNotes(notes, filters);
+});
