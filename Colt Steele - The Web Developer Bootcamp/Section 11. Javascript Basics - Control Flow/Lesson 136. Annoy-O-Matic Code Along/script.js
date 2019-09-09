46a8e872913cb610ad636d4923b4abdcff6e5534
John Dinh <johnduydinh@gmail.com>
2019-09-08 21:40:59 -0400

    Completed WDBC Section 11 - Lesson 136

---
+++ Colt Steele - The Web Developer Bootcamp/Section 11. Javascript Basics - Control Flow/Lesson 136. Annoy-O-Matic Code Along/script.js
@@ -0,0 +1,17 @@
+const bodyEl = document.querySelector('body');
+
+function appendH1() {
+    const h1 = document.createElement('h1');
+    h1.textContent = "Yay, we finally made it!";
+    bodyEl.appendChild(h1);
+}
+
+function annoy(input) {
+    if (input.toLowerCase().indexOf('yes') > 0 || input.toLowerCase().indexOf('yeah') > 0) {
+        appendH1();
+    } else {
+        annoy(prompt('Are we there yet?'));
+    }
+}
+
+annoy(prompt('Are we there yet?'));
