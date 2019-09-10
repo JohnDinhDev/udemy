53d81a943ccfeb2964f404ff309b34d06638b8ff
John Dinh <johnduydinh@gmail.com>
2019-09-10 19:06:45 -0400

    Complete JS Hard Level WDBC Color Game Project

--- Colt Steele - The Web Developer Bootcamp/Section 17. Color Game Project/script.js
+++ Colt Steele - The Web Developer Bootcamp/Section 17. Color Game Project/script.js
@@ -1,9 +1,19 @@
 const rgbNumbersEl = document.querySelector('.rgb-numbers');
 const squaresEl = document.querySelectorAll('.square');
+const messageEl = document.querySelector('.message');
+const headerEl = document.querySelector('.heading')
+const newGameBtn = document.querySelector('#new-game');
 
+let difficultyNumber = 6;
 let colorData = setColorData();
 let colorAnswer = generateAnswer();
-
+let winningWords = [
+    'Nice Job!',
+    'Fantastic!',
+    'You\'re awesome!',
+    'Great!'
+]
+let gameState = false;
 
 
 function generateRandomRGB() {
@@ -22,22 +32,49 @@ function generateAnswer() {
 // Sets colorData data
 function setColorData() {
     let arr = [];
-    for (let squares of squaresEl) {
+    for (let i = 0; i < difficultyNumber; i++) {
         arr.push(generateRandomRGB());
     }
     return arr;
 }
 
 function init() {
+    colorData = setColorData();
     // Init colors onto squares
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < difficultyNumber; i++) {
         squaresEl[i].style.backgroundColor = colorData[i];
     }
+    colorAnswer = generateAnswer();
 
     rgbNumbersEl.textContent = colorAnswer.toUpperCase();
-
 }
 
-init();
+function randomWinningWord() {
+   let index = Math.floor(Math.random() * winningWords.length);
+   return winningWords[index];
+}
 
-console.log(colorAnswer)
+// event listener for squares
+squaresEl.forEach(square => {
+    square.addEventListener('click', e => {
+        if (square.style.backgroundColor === colorAnswer && !gameState) {
+            gameState = !gameState;
+            for (let square of squaresEl) {
+                square.style.backgroundColor = colorAnswer;
+            }
+            headerEl.style.backgroundColor = colorAnswer;
+            messageEl.textContent = randomWinningWord();
+        } else if (!gameState) {
+            square.style.backgroundColor = '#201F20';
+        }
+    });
+});
+
+newGameBtn.addEventListener('click', e => {
+    headerEl.backgroundColor = 'rgb(138, 157, 218)';
+    messageEl.textContent = '';
+    gameState = !gameState;
+    init();
+})
+
+init();
