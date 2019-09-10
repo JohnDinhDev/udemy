b265dc55d9b4add1ac79e185f6ca257bb2d50ffd
John Dinh <johnduydinh@gmail.com>
2019-09-10 18:13:34 -0400

    Complete rgb answer message WDBC Color Project Game

--- Colt Steele - The Web Developer Bootcamp/Section 17. Color Game Project/script.js
+++ Colt Steele - The Web Developer Bootcamp/Section 17. Color Game Project/script.js
@@ -1,17 +1,10 @@
 const rgbNumbersEl = document.querySelector('.rgb-numbers');
 const squaresEl = document.querySelectorAll('.square');
 
-let colorData = [];
+let colorData = setColorData();
+let colorAnswer = generateAnswer();
 
-// Init random color data
-for (let squares of squaresEl) {
-    colorData.push(generateRandomRGB());
-}
 
-// Init colors onto squares
-for (let i = 0; i < 6; i++) {
-    squaresEl[i].style.backgroundColor = colorData[i];
-}
 
 function generateRandomRGB() {
     const r = Math.floor(Math.random() * 255);
@@ -19,3 +12,32 @@ function generateRandomRGB() {
     const b = Math.floor(Math.random() * 255);
     return `rgb(${r}, ${g}, ${b})`;
 }
+
+//function to pick random answer
+function generateAnswer() {
+    const randomAnswer = Math.floor(Math.random() * colorData.length);
+    return colorData[randomAnswer];
+}
+
+// Sets colorData data
+function setColorData() {
+    let arr = [];
+    for (let squares of squaresEl) {
+        arr.push(generateRandomRGB());
+    }
+    return arr;
+}
+
+function init() {
+    // Init colors onto squares
+    for (let i = 0; i < 6; i++) {
+        squaresEl[i].style.backgroundColor = colorData[i];
+    }
+
+    rgbNumbersEl.textContent = colorAnswer.toUpperCase();
+
+}
+
+init();
+
+console.log(colorAnswer)
