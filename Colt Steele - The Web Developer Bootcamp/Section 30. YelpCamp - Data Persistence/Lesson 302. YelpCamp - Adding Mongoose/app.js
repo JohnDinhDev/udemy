133eb79889f47948cfcfb158c13ddd599cdfbc41
John Dinh <johnduydinh@gmail.com>
2019-09-15 08:40:48 -0400

    Completed Section 30 - Lesson 303

--- Colt Steele - The Web Developer Bootcamp/Section 30. YelpCamp - Data Persistence/Lesson 302. YelpCamp - Adding Mongoose/app.js
+++ Colt Steele - The Web Developer Bootcamp/Section 30. YelpCamp - Data Persistence/Lesson 302. YelpCamp - Adding Mongoose/app.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-mongoose.connect('mongodb://localhost/yelp_camp', {
+mongoose.connect('mongodb://localhost:27017/yelp_camp', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
