fff6c119bb22c9445c76e8039ce0284dbf9c85fa
John Dinh <johnduydinh@gmail.com>
2019-09-21 10:43:44 -0400

    Complete WDBC Section 34 - Lesson 339

---
+++ Colt Steele - The Web Developer Bootcamp/Section 34. Authentication/Lesson 339. Secrate Page Code Along Pt. 4/models/user.js
@@ -0,0 +1,13 @@
+const passportLocalMongoose = require('passport-local-mongoose');
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
