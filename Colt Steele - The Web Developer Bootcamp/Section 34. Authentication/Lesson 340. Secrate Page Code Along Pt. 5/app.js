de271ca75494b9de4226b02686b25a32bead1c4e
John Dinh <johnduydinh@gmail.com>
2019-09-21 11:20:36 -0400

    Completed WDBC Section 34 - Lesson 340

---
+++ Colt Steele - The Web Developer Bootcamp/Section 34. Authentication/Lesson 340. Secrate Page Code Along Pt. 5/app.js
@@ -0,0 +1,90 @@
+const passportLocalMongoose = require('passport-local-mongoose');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+mongoose.connect('mongodb://localhost:27017/auth_demo_app', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+
+app.use(require('express-session')({
+    secret: 'Send me some pies please 4454',
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.set('view engine', 'ejs');
+
+//=================
+// ROUTES
+//=================
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/secret', isLoggedIn, (req, res) => {
+    res.render('secret');
+});
+
+// Auth Routes
+app.get('/register', (req, res) => {
+    res.render('register', { errorMessage: false });
+});
+
+app.post('/register', (req, res) => {
+    User.register(new User({ username: req.body.username }), req.body.password, (err, user) => {
+        if (err) {
+            res.render('register', { errorMessage: err.message });
+        } else {
+            passport.authenticate('local')(req, res, () => {
+                res.redirect('/secret');
+            });
+        }
+    });
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/secret',
+    failureRedirect: '/login',
+}), (req, res) => {
+
+});
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        res.redirect('/');
+    }
+}
+
+app.listen(port, () => {
+    console.log(`Server has started on port ${port}`);
+});
