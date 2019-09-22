81aed5887a5cef415a362984010e2c620640f8b2
John Dinh <johnduydinh@gmail.com>
2019-09-22 19:55:56 -0400

    Complete WDBC Section 37 - Lesson 354

---
+++ Colt Steele - The Web Developer Bootcamp/Section 37. YelpCamp - Update and Destroy/Lesson 354. Campground Authorization Part 2/routes/comments.js
@@ -0,0 +1,36 @@
+const Campground = require('../models/campground');
+const Comment = require('../models/comment');
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
+
+router.get('/new', isLoggedIn, (req, res) => {
+    // find campground by id
+    Campground.findById(req.params.id, (err, campground) => {
+        if (err) return console.error(err);
+        res.render('comments/new', { campground: campground });
+    });
+});
+
+router.post('/', isLoggedIn, (req, res) => {
+    Campground.findById(req.params.id, (err, campground) => {
+        if (err) return console.error(err);
+        Comment.create(req.body.comment, (err, comment) => {
+            comment.author.id = req.user._id;
+            comment.author.username = req.user.username;
+            comment.save();
+            if (err) return console.error(err);
+            campground.comments.push(comment);
+            campground.save();
+            res.redirect(`/campgrounds/${campground._id}`);
+        })
+    });
+});
+
+module.exports = router;
