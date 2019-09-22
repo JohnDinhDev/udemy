5e409e5fa5af6d811635a502a4b2ee6840e82f9d
John Dinh <johnduydinh@gmail.com>
2019-09-22 00:35:43 -0400

    Complete WDBC Section 36 - Lesson 348

---
+++ Colt Steele - The Web Developer Bootcamp/Section 36. YelpCamp - Cleaning Up/Lesson 348. YelpCamp - User Associations - Campground/routes/campgrounds.js
@@ -0,0 +1,62 @@
+const Campground = require('../models/campground');
+const express = require('express');
+const router = express.Router();
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
+
+// INDEX - show all campgrounds
+router.get('/', (req, res) => {
+    Campground.find({}, (err, campgrounds) => {
+        if (err) return console.error(err);
+        res.render('campgrounds/index', {
+            campgrounds: campgrounds,
+        });
+    })
+
+});
+
+router.get('/new', isLoggedIn, (req, res) => {
+    res.render('campgrounds/new');
+});
+
+// CREATE - add new campground to DB
+router.post('/', isLoggedIn, (req, res) => {
+    const name = req.body.name;
+    const image = req.body.image;
+    const description = req.body.description;
+    const author = {
+        id: req.user._id,
+        username: req.user.username,
+    };
+    const newCampground = new Campground({
+        name: name,
+        img: image,
+        description: description,
+        author: author
+    });
+    newCampground.save((err, campground) => {
+        if (err) return console.error(err);
+        console.log(`${campground.name} has been saved`);
+        res.redirect('/campgrounds');
+    });
+});
+
+// SHOW - shows more info about one campground
+router.get('/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    Campground.findById(id)
+        .populate('comments')
+        .exec((err, campground) => {
+            if (err) return console.error(err);
+            res.render('campgrounds/show', { campground: campground });
+        });
+});
+
+module.exports = router;
