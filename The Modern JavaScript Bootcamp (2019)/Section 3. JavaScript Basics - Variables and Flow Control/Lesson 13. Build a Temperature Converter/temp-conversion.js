494c47d7aba082a4c7dbbe81869b4202efee123f
John Dinh <johnduydinh@gmail.com>
2019-09-24 02:34:12 -0400

    Complete TMJB Section 3 - Lesson 13

---
+++ The Modern JavaScript Bootcamp (2019)/Section 3. JavaScript Basics - Variables and Flow Control/Lesson 13. Build a Temperature Converter/temp-conversion.js
@@ -0,0 +1,16 @@
+//=========================================================================================
+//     Challenge
+//=========================================================================================
+// 1. Create variables for fahrenheit, celsius, and kelvin
+// 2. Set fahrenheit to a number, and convert that to celsius and kelvin
+// 3. Set converted numbers respectively to the variable names
+
+let fahrenheit = 37;
+
+let celsius = (fahrenheit - 32) * (5/9);
+
+let kelvin = celsius + 273.15;
+
+console.log(`${fahrenheit} in fahrenheit is also`);
+console.log(`${celsius} in celsius`);
+console.log(`And ${kelvin} in kelvin`);
