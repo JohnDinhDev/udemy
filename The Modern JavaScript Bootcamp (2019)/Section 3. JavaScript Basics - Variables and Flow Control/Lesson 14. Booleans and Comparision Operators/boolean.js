24cebbc761246fd2ed650db5c77de625d7f7fa73
John Dinh <johnduydinh@gmail.com>
2019-09-24 02:40:58 -0400

    Complete TMJB Section 3 - Lesson 14

---
+++ The Modern JavaScript Bootcamp (2019)/Section 3. JavaScript Basics - Variables and Flow Control/Lesson 14. Booleans and Comparision Operators/boolean.js
@@ -0,0 +1,21 @@
+//=========================================================================================
+//     Challenge
+//=========================================================================================
+
+// 1. Create age variable set to your age
+// 2. Calculate is child - if they are 7 or under
+// 3. Calculate is seinior - if they are 65 or older
+// 4. Print is child value
+// 5. Print is senior value
+
+// Note. isChild and isSenior variables should be booleans
+// they should be equal to true or false;
+
+let age = 21;
+
+let isChild = age <= 7;
+
+let isSenior = age >= 65;
+
+console.log(isChild);
+console.log(isSenior);
