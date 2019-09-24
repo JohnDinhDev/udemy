f83b6c40a322732c1c8e9c81c6157ed875ce5c69
John Dinh <johnduydinh@gmail.com>
2019-09-24 02:51:47 -0400

    Complete TMJB Section 3 - Lesson 16

---
+++ The Modern JavaScript Bootcamp (2019)/Section 3. JavaScript Basics - Variables and Flow Control/Lesson 16. Advanced If Statements/boolean-advanced.js
@@ -0,0 +1,18 @@
+//=========================================================================================
+//     Challenge
+//=========================================================================================
+
+let temp = 45;
+
+// Make if statement that tells you if it is
+// Freezing out
+// Hot out
+// Or just normal
+
+if (temp >= 85) {
+    console.log("It is hot outside");
+} else if (temp <= 36) {
+    console.log("It is literally freezing out");
+} else {
+    console.log("The temperature is fine out");
+}
