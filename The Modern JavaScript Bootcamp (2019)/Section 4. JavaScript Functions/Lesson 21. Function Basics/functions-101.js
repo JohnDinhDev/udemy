d9264a5536d653c7c10c62136598820da8c5d39b
John Dinh <johnduydinh@gmail.com>
2019-09-24 12:28:45 -0400

    Complete TMJB Section 4 - Lesson 21

---
+++ The Modern JavaScript Bootcamp (2019)/Section 4. JavaScript Functions/Lesson 21. Function Basics/functions-101.js
@@ -0,0 +1,12 @@
+//=========================================================================================
+//     Challenge
+//=========================================================================================
+
+// Create a function convertFahrenheitToCelsius
+
+function convertFahrenheitToCelsius(fahrenheitDegrees) {
+    return (fahrenheitDegrees - 32) * (5 / 9);
+}
+
+console.log(convertFahrenheitToCelsius(32));
+console.log(convertFahrenheitToCelsius(68));
