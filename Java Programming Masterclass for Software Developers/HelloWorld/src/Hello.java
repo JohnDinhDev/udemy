f96fcfb775790e5eeaf55786d1b152db2415e01c
John Dinh <johnduydinh@gmail.com>
2019-09-29 23:24:10 -0400

    Start using expressions

--- Java Programming Masterclass for Software Developers/HelloWorld/src/Hello.java
+++ Java Programming Masterclass for Software Developers/HelloWorld/src/Hello.java
@@ -1,9 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello John!");
-
         int myFirstNumber = (10 + 5) + (2 * 10);
-
-        System.out.println(myFirstNumber);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = myTotal - 1000;
+        System.out.println("Hello John!");
+        System.out.println(myLastOne);
     }
 }
