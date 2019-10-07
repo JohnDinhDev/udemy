8b3b532b8203ec8d89b800f140a2219aa6c9defe
John Dinh <johnduydinh@gmail.com>
2019-10-07 08:55:29 -0400

    refactor geolocation to componentDidMount

--- stephen-grider-react/seasons/src/index.js
+++ stephen-grider-react/seasons/src/index.js
@@ -4,14 +4,13 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = { lat: null, errorMessage: "" };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({lat: position.coords.latitude});
-      },
-      err => this.setState( {errorMessage: err.message} )
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
