ce7200952cd30e51bad8a67051ca78e48b96863c
John Dinh <johnduydinh@gmail.com>
2019-10-07 14:28:58 -0400

    create loader spinner

--- stephen-grider-react/seasons/src/index.js
+++ stephen-grider-react/seasons/src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-
   state = { lat: null, errorMessage: "" };
 
   componentDidMount() {
@@ -15,16 +15,19 @@ class App extends React.Component {
 
   render() {
     if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>
+      return <div>Error: {this.state.errorMessage}</div>;
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading!</div>
+    return (
+      <div>
+        <Spinner />
+      </div>
+    );
   }
 }
-
 
 ReactDOM.render(<App />, document.querySelector("#root"));
