b2cf4aebf7b8423c89d4779a38bd693d95f5dd0d
John Dinh <johnduydinh@gmail.com>
2019-10-09 15:47:04 -0400

    refactor axios get request

--- stephen-grider-react/pics/src/components/App.js
+++ stephen-grider-react/pics/src/components/App.js
@@ -1,17 +1,14 @@
-import axios from "axios";
 import React from "react";
 import SearchBar from "./SearchBar";
+import unsplash from "./../components/api/unsplash";
 
 class App extends React.Component {
 
   state = { images: [] };
 
   onSearchSubmit = async term => {
-    const response = await axios.get("https://api.unsplash.com/search/photos", {
+    const response = await unsplash.get("/search/photos", {
       params: { query: term },
-      headers: {
-        Authorization: "Client-ID 7742fd36b619bf1cacecf4711d208a7823ffcc4c6f11cf04ccf0f6ba7103e7cd"
-      }
     });
     this.setState({ images: response.data.results });
   }
