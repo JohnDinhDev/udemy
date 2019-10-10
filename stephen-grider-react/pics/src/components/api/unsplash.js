8297b01f034d21624c2e9e510565cf1a5aa207da
John Dinh <johnduydinh@gmail.com>
2019-10-10 18:47:09 -0400

    Add initial css grid layout for pics app

--- stephen-grider-react/pics/src/components/api/unsplash.js
+++ stephen-grider-react/pics/src/components/api/unsplash.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID API_KEY"
+    Authorization:
+      "Client-ID 7742fd36b619bf1cacecf4711d208a7823ffcc4c6f11cf04ccf0f6ba7103e7cd"
   }
 });
