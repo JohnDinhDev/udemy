2465b4d42b778d92adbcf94440ad0856fbd3a97a
John Dinh <johnduydinh@gmail.com>
2019-10-11 20:26:40 -0400

    prevent default submit

--- stephen-grider-react/videos/src/components/SearchBar.js
+++ stephen-grider-react/videos/src/components/SearchBar.js
@@ -3,12 +3,21 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
-  onInputChange = () => {};
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    //TODO: Make sure we call
+    // callback from parent component
+  };
 
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label htmlFor="search-input">Video Search</label>
             <input
